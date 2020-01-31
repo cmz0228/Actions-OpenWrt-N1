58dea538493a83b834b583e58dcba69f44d6061c
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-01-31 21:54:11 +0800

    Update pi.sh

--- pi.sh
+++ pi.sh
@@ -6,6 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#sed -i '3a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#./scripts/feeds update -a && ./scripts/feeds install -a
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i '3a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
