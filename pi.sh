04369ecd8817606c3614a0e4f82450150a66a4be
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-01-31 22:05:48 +0800

    Update pi.sh

--- pi.sh
+++ pi.sh
@@ -7,5 +7,15 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i '3a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+#sed -i '3a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#./scripts/feeds update -a && ./scripts/feeds install -a
+
+echo '下载新的主题lignt and night'
+git clone https://github.com/Aslin-Ameng/luci-theme-Light.git package/luci-theme-Light
+git clone https://github.com/Aslin-Ameng/luci-theme-Night.git package/luci-theme-Night
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/Leo-Jo-My/luci-theme-leo.git package/luci-theme-leo
+
+echo '下载passwall'
+git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
+
